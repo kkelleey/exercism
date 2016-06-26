d9082d6d610482c3de8a8a9aea72221983e7b04e
Karen Kelley <karenkelley11@gmail.com>
2016-06-25 23:23:42 -0500

    Making solution more pythony

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,5 +1,4 @@
+from string import ascii_lowercase
+
 def is_pangram(string):
-    for i in range(97, 97 + 26):
-        if not (chr(i) in string.lower()):
-            return False
-    return True
+    return all(letter in string.lower() for letter in ascii_lowercase)
