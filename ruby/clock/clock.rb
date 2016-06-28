1bdfa97c710668d5258b29c8919b45f6771a3390
Karen Kelley <karenkelley11@gmail.com>
2016-06-28 16:57:35 -0500

    Cleaning up clock.rb

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,27 +1,39 @@
+class Fixnum
+  def format!
+    to_s.rjust(2, '0')
+  end
+end
+
 class Clock
   def initialize(hour, minute)
-    @hour = hour
-    @minute = minute
+    @minutes = hour * 60 + minute
   end
 
   def to_s
-    "#{@hour.to_s.rjust(2, '0')}:#{@minute.to_s.rjust(2, '0')}"
+    "#{hour.format!}:#{minute.format!}"
   end
 
   def +(other)
-    @hour = (@hour + (other + @minute) / 60) % 24
-    @minute = (other + @minute) % 60
+    @minutes += other
     self
   end
 
   def self.at(hour, minute)
-    hour = (hour + minute / 60) % 24
-    minute = minute % 60
     new(hour, minute)
   end
 
   def ==(other)
     to_s == other.to_s
+  end
+
+  private
+
+  def hour
+    (@minutes / 60) % 24
+  end
+
+  def minute
+    @minutes % 60
   end
 end
 
