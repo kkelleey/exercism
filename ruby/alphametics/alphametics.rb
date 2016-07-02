d607aa1533d72c3fc2ee0403c09791bb3d583e37
Karen Kelley <karenkelley11@gmail.com>
2016-07-01 21:15:08 -0500

    More refactoring

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,59 +1,25 @@
-class String
-  def to_number(map)
-    gsub(/[a-zA-Z]/, map).to_i
-  end
-end
-
 class Alphametics
-  WORD_OPERATOR_REST_REGEX = /
-                              (?<word>[a-zA-Z]+)
-                              \s?
-                              (?<operator>[*+])?
-                              \s?
-                              (?<rest>.+)?
-                            /x
-
   def solve(string)
-    @string = replace_exponent(string)
+    @string = string.gsub('^', '**')
 
-    (0..9).to_a.combination(uniq_chars.length) do |combination|
-      combination.permutation do |permutation|
-        map = uniq_chars.zip(permutation).to_h
-        next if words.any? { |w| map[w[0]] == 0 }
-        return map if puzzle_solved?(map)
-      end
-    end
-    nil
+    possible_maps.find { |map| puzzle_solved?(map) }
   end
 
   private
 
-  def words
-    @words ||= @string.scan(/[A-Za-z]+/)
-  end
+  def possible_maps
+    uniq_chars = @string.scan(/[[:alpha:]]/).uniq
+    permutations = (0..9).to_a.permutation(uniq_chars.length)
 
-  def uniq_chars
-    @string.scan(/[A-Za-z]/).uniq
+    permutations.map do |permutation|
+      uniq_chars.zip(permutation).to_h
+    end
   end
 
   def puzzle_solved?(map)
-    left_side, right_side = @string.split('==')
-    value_of(right_side, map) == value_of(left_side, map)
-  end
-
-  def value_of(string, map)
-    match = string.strip.match(WORD_OPERATOR_REST_REGEX)
-    value_of_word = match['word'].to_number(map)
-    return value_of_word if match['operator'].nil?
-    value_of_rest_of_string = value_of(match['rest'], map)
-    value_of_word.send(match['operator'], value_of_rest_of_string)
-  end
-
-  def replace_exponent(string)
-    return string unless string.include? '^'
-    base = string.scan(/(\w+)\s?\^/)[0][0]
-    exponent = string.scan(/\^\s?(\d)/)[0][0].to_i
-    string.gsub(/\w+\s?\^\s?\d/, ([base] * exponent).join(' * '))
+    string = @string.gsub(/[[:alpha:]]/, map)
+    return false if string =~ /\b0\d+/
+    eval(string)
   end
 end
 
