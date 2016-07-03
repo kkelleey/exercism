262b788910affe68fd6b55b41e29c798f6833478
Karen Kelley <karenkelley11@gmail.com>
2016-07-03 16:14:35 -0500

    Adding hexadecimal

---
+++ ruby/hexadecimal/hexadecimal.rb
@@ -0,0 +1,25 @@
+class Hexadecimal
+  HEX_VALUES = [*'0'..'9', *'A'..'F'].freeze
+
+  def initialize(string)
+    @string = string
+  end
+
+  def to_decimal
+    return 0 if invalid_hex?
+    @string.chars.reverse.each_with_index.reduce(0) do |a, (char, index)|
+      a + hex_value(char) * 16**index
+    end
+  end
+
+  private
+
+  def invalid_hex?
+    @string =~ /[^0-9a-fA-F]/
+  end
+
+  def hex_value(char)
+    # char =~ /\d/ ? char.to_i : HEX_VALUES[char.upcase]
+    HEX_VALUES.index(char.upcase)
+  end
+end
