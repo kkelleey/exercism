d067058f366d80e775da930d09a224ab8f97b85c
Karen Kelley <karenkelley11@gmail.com>
2016-07-12 20:28:53 -0500

    Updating hexidecimal

--- ruby/hexadecimal/hexadecimal.rb
+++ ruby/hexadecimal/hexadecimal.rb
@@ -1,24 +1,9 @@
 class Hexadecimal
-  HEX_VALUES = [*'0'..'9', *'A'..'F'].freeze
-
   def initialize(string)
     @string = string
   end
 
   def to_decimal
-    return 0 if invalid_hex?
-    @string.chars.reverse.each_with_index.reduce(0) do |a, (char, index)|
-      a + hex_value(char) * 16**index
-    end
-  end
-
-  private
-
-  def invalid_hex?
-    @string =~ /[^0-9a-fA-F]/
-  end
-
-  def hex_value(char)
-    HEX_VALUES.index(char.upcase)
+    @string =~ /[^0-9a-fA-F]/ ? 0 : @string.to_i(16)
   end
 end
